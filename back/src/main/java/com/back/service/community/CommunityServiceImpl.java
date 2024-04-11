2b2de2e97c36de3eab1c25fb6023ebff9cd650aa
dis2211 <0221kmac@gmail.com>
2024-04-11 16:20:06 +0900

    disabled community

--- back/src/main/java/com/back/service/community/CommunityServiceImpl.java
+++ back/src/main/java/com/back/service/community/CommunityServiceImpl.java
@@ -1,81 +1,81 @@
-package com.back.service.community;
-
-
-import com.back.domain.community.Community;
-import com.back.dto.community.CommunityDTO;
-import com.back.repository.community.CommunityRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@RequiredArgsConstructor
-@Service
-@Transactional
-@Log4j2
-
-public class CommunityServiceImpl implements CommunityService {
-
-    private final CommunityRepository communityRepository;
-
-    @Override
-    public Long regCommunity(CommunityDTO communityDTO) {
-        Community community = dtoToEnity(communityDTO);
-        Community result = communityRepository.save(community);
-        return result.getCommunityBno();
-
-    }
-
-    private Community dtoToEnity(CommunityDTO communityDTO) {
-
-        Community community = Community.builder()
-                .communityBno(communityDTO.getCommunityBno())
-                .communityTitle(communityDTO.getCommunityTitle())
-                .communityContent(communityDTO.getCommunityContent())
-                .communityWriter(communityDTO.getCommunityWriter())
-                .build();
-
-        return community;
-    }
-
-//    @Override
-//    public CommunityDTO getCommunity(Long communityBno) {
-//        Optional<Community> result = communityRepository.selectOne(communityBno);
-//        Community community = result.orElseThrow();
-//        CommunityDTO communityDTO = entityToDTO(community);
+//package com.back.service.community;
 //
+//
+//import com.back.domain.community.Community;
+//import com.back.dto.community.CommunityDTO;
+//import com.back.repository.community.CommunityRepository;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@RequiredArgsConstructor
+//@Service
+//@Transactional
+//@Log4j2
+//
+//public class CommunityServiceImpl implements CommunityService {
+//
+//    private final CommunityRepository communityRepository;
+//
+//    @Override
+//    public Long regCommunity(CommunityDTO communityDTO) {
+//        Community community = dtoToEnity(communityDTO);
+//        Community result = communityRepository.save(community);
+//        return result.getCommunityBno();
+//
+//    }
+//
+//    private Community dtoToEnity(CommunityDTO communityDTO) {
+//
+//        Community community = Community.builder()
+//                .communityBno(communityDTO.getCommunityBno())
+//                .communityTitle(communityDTO.getCommunityTitle())
+//                .communityContent(communityDTO.getCommunityContent())
+//                .communityWriter(communityDTO.getCommunityWriter())
+//                .build();
+//
+//        return community;
+//    }
+//
+////    @Override
+////    public CommunityDTO getCommunity(Long communityBno) {
+////        Optional<Community> result = communityRepository.selectOne(communityBno);
+////        Community community = result.orElseThrow();
+////        CommunityDTO communityDTO = entityToDTO(community);
+////
+////        return communityDTO;
+////    }
+//
+//    private CommunityDTO entityToDTO(Community community) {
+//
+//        CommunityDTO communityDTO = CommunityDTO.builder()
+//                .communityBno(community.getCommunityBno())
+//                .communityTitle(community.getCommunityTitle())
+//                .communityContent(community.getCommunityContent())
+//                .communityWriter(community.getCommunityWriter())
+//                .build();
 //        return communityDTO;
 //    }
-
-    private CommunityDTO entityToDTO(Community community) {
-
-        CommunityDTO communityDTO = CommunityDTO.builder()
-                .communityBno(community.getCommunityBno())
-                .communityTitle(community.getCommunityTitle())
-                .communityContent(community.getCommunityContent())
-                .communityWriter(community.getCommunityWriter())
-                .build();
-        return communityDTO;
-    }
-
-    @Override
-    @Transactional
-    public void modCommunity(CommunityDTO communityDTO) {
-        Optional<Community> optionalCommunity = communityRepository.findById(communityDTO.getCommunityBno());
-
-        Community community = optionalCommunity.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
-        community.changeTitle(community.getCommunityTitle());
-        community.changeContent(communityDTO.getCommunityContent());
-
-        communityRepository.save(community);
-    }
-
-    @Override
-    public void delCommunity(Long communityBno) {
-        communityRepository.updateToDelete(communityBno);
-    }
-
-
-}
+//
+//    @Override
+//    @Transactional
+//    public void modCommunity(CommunityDTO communityDTO) {
+//        Optional<Community> optionalCommunity = communityRepository.findById(communityDTO.getCommunityBno());
+//
+//        Community community = optionalCommunity.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
+//        community.changeTitle(community.getCommunityTitle());
+//        community.changeContent(communityDTO.getCommunityContent());
+//
+//        communityRepository.save(community);
+//    }
+//
+//    @Override
+//    public void delCommunity(Long communityBno) {
+//        communityRepository.updateToDelete(communityBno);
+//    }
+//
+//
+//}
