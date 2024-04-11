7ec3e16ea67757abf223527506d8109ec5853da5
dis2211 <0221kmac@gmail.com>
2024-04-11 15:16:09 +0900

    test
    test

--- back/src/main/java/com/back/domain/member/MemberRole.java
+++
@@ -1,6 +0,0 @@
-package com.back.domain.member;
-
-public enum MemberRole {
-
-    USER, ADMIN;
-}
--- back/src/main/java/com/back/util/CustomJWTException.java
+++
@@ -1,9 +0,0 @@
-package com.back.util;
-
-public class CustomJWTException extends RuntimeException{
-
-    public CustomJWTException(String msg){
-        super(msg);
-    }
-
-}
