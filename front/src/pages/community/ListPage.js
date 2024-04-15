eed6137cedf3e21d5f8991911ae3fdb4e76244be
myj1217 <myj1217@naver.com>
2024-04-15 16:30:54 +0900

    Update ListPage.js
    Modify Path

--- front/src/pages/community/ListPage.js
+++ front/src/pages/community/ListPage.js
@@ -1,17 +1,20 @@
-import { useSearchParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
 import ListCommunityComponent from "../../components/community/ListCommunityComponent";
+import BasicMenu from "../../components/menus/BasicMenu";
+import { FaComment } from "react-icons/fa";
 // import { FaComment } from "react-icons/fa";
 
 const ListPage = () => {
-  const [queryParams] = useSearchParams();
+  // const [queryParams] = useSearchParams();
 
-  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
-  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+  // const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  // const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
 
   return (
     <div className="flex flex-col w-full">
+      <BasicMenu />
       <div className="h-11 bg-gray-700 text-white flex items-center pl-8 sticky top-0 z-55">
-        {/* <FaComment className="w-6 h-6 mr-2" /> 커뮤니티 */}
+        <FaComment className="w-6 h-6 mr-2" /> 커뮤니티
       </div>
       <div className="w-full h-full">
         <ListCommunityComponent />
