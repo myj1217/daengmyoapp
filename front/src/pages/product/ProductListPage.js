8380b6991a7e5aa3ee9fb75951af2d983937b5ca
myj1217 <myj1217@naver.com>
2024-04-15 16:31:53 +0900

    Update ProductListPage.js
    Modify Path

--- front/src/pages/product/ProductListPage.js
+++ front/src/pages/product/ProductListPage.js
@@ -1,9 +1,11 @@
+import BasicMenu from "../../components/menus/BasicMenu";
 import ProductListComponent from "../../components/product/ProductListComponent";
 import { FaGift } from "react-icons/fa";
 
 const ProductListPage = () => {
   return (
     <div className="flex flex-col w-full">
+      <BasicMenu />
       <div className="h-11 bg-gray-700 text-white flex items-center pl-8 sticky top-0 z-50">
         <FaGift className="w-6 h-6 mr-2" />
         상품 목록
