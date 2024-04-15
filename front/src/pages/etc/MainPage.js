1f127237da708b39c606baaf71126151a1241185
Glenn <112257932+jy02145709@users.noreply.github.com>
2024-04-15 14:42:38 +0900

    Update MainPage.js

--- front/src/pages/etc/MainPage.js
+++ front/src/pages/etc/MainPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Basicmenu from "../../components/menus/Basicmenu";
+import MainBanner from "../../components/main/MainBanner";
+import Introduction from "../../components/main/Introduction";
+import MissingPet from "../../components/main/MissingPet";
+import ProductSection from "../../components/main/ProductSection";
+import Reviews from "../../components/main/Reviews";
+import Footer from "../../components/main/Footer";
+import { div } from "@tensorflow/tfjs";
 
 const MainPage = () => {
   return (
     <div>
-      <h1>메인페이지 입니다.</h1>
+      <Basicmenu />
+      <MainBanner />
+      <Introduction />
+      <div className="text-center text-4xl font-bold mb-6">
+        우리 아이를 찾고 있어요
+      </div>
+      <MissingPet />
+
+      <Footer />
     </div>
   );
 };
