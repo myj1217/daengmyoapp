c68fcf0ce53938c091b810c2156bd1310cba27de
myj1217 <myj1217@naver.com>
2024-04-15 15:57:21 +0900

    Update MainPage.js
    Modify Path

--- front/src/pages/etc/MainPage.js
+++ front/src/pages/etc/MainPage.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Basicmenu from "../../components/menus/Basicmenu";
+import BasicMenu from "../../components/menus/BasicMenu";
 import MainBanner from "../../components/main/MainBanner";
 import Introduction from "../../components/main/Introduction";
 import MissingPet from "../../components/main/MissingPet";
-import ProductSection from "../../components/main/ProductSection";
-import Reviews from "../../components/main/Reviews";
 import Footer from "../../components/main/Footer";
-import { div } from "@tensorflow/tfjs";
 
 const MainPage = () => {
   return (
     <div>
-      <Basicmenu />
+      <BasicMenu />
       <MainBanner />
       <Introduction />
       <div className="text-center text-4xl font-bold mb-6">
