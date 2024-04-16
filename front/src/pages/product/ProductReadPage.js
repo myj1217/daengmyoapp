dc17c428d3a0c657e4853e93ff07be4874e1412d
myj1217 <myj1217@naver.com>
2024-04-16 16:04:26 +0900

    Add BasicMenu

--- front/src/pages/product/ProductReadPage.js
+++ front/src/pages/product/ProductReadPage.js
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom";
 import ProductReadComponent from "../../components/product/ProductReadComponent";
+import BasicMenu from "../../components/menus/BasicMenu";
 
 const ProductReadPage = () => {
   const { pno } = useParams();
 
   return (
     <div className="p-4 w-full bg-white">
+      <BasicMenu />
       <ProductReadComponent pno={pno} />
     </div>
   );
