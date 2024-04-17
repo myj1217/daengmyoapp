60671015357db2bb9572ffc69025dfbf26257517
myj1217 <myj1217@naver.com>
2024-04-17 16:23:08 +0900

    Add List Button

--- front/src/components/product/ProductAddComponent.js
+++ front/src/components/product/ProductAddComponent.js
@@ -44,6 +44,7 @@ const ProductAddComponent = () => {
     formData.append("price", product.price);
     formData.append("artist", loginState.nickname);
 
+    console.log("formData");
     console.log(formData);
 
     setFetching(true);
@@ -52,6 +53,10 @@ const ProductAddComponent = () => {
       setFetching(false);
       setResult(data.result);
     });
+  };
+
+  const handleClickList = () => {
+    navigate({ pathname: "../" });
   };
 
   const closeModal = () => {
@@ -126,10 +131,17 @@ const ProductAddComponent = () => {
         <div className="relative mb-4 flex p-4 flex-wrap items-stretch">
           <button
             type="button"
-            className="rounded p-4 w-36 bg-gray-800 text-xl  text-white "
+            className="rounded mx-2 p-4 w-36 bg-gray-800 text-xl  text-white "
             onClick={handleClickAdd}
           >
             추가하기
+          </button>
+          <button
+            type="button"
+            className="rounded mx-2 p-4 w-36 bg-gray-800 text-xl  text-white "
+            onClick={handleClickList}
+          >
+            목록으로
           </button>
         </div>
       </div>
