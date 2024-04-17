9b3c42a51ec3d8912005324fbdf2fa2c29b9fc7d
myj1217 <myj1217@naver.com>
2024-04-17 17:32:49 +0900

    Add Cart Menu

--- front/src/components/menus/BasicMenu.js
+++ front/src/components/menus/BasicMenu.js
@@ -1,20 +1,33 @@
-import React from "react";
-import { Link ,useLocation} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import useCustomLogin from "../../hooks/useCustomLogin";
 import image from "../../images/logo.png";
+import useCustomCart from "../../hooks/useCustomCart";
+
 const BasicMenu = () => {
   const { isLogin, doLogout, moveToPath } = useCustomLogin();
   const location = useLocation();
-  
+  const { refreshCart, cartItems } = useCustomCart();
+
   const clickLogout = () => {
     doLogout();
     moveToPath("/");
   };
 
+  // useEffect(() => {
+  //   if (isLogin) {
+  //     refreshCart();
+  //   }
+  // }, [isLogin]);
+
   const isMyPage = location.pathname === "/member/mypage";
-  
+
   return (
-    <header className={`flex bg-green-50 text-green-800 p-4 w-full h-20 top-0 z-50 ${isMyPage ? "" : "sticky shadow-md"}`}>
+    <header
+      className={`flex bg-green-50 text-green-800 p-4 w-full h-20 top-0 z-50 ${
+        isMyPage ? "" : "sticky shadow-md"
+      }`}
+    >
       <div className="container mx-auto flex justify-between items-center h-full w-full">
         <Link to="/" className="text-lg font-bold">
           <img src={image} alt="logo" className="w-32 h-auto"></img>
@@ -70,20 +83,35 @@ const BasicMenu = () => {
               </Link>
             </li>
             {isLogin ? (
-            <li
-              onClick={clickLogout}
-              className="hover:text-amber-200 transition-colors duration-300 cursor-pointer"
-            >
-              로그아웃
-            </li>
-          ) : (
-            <Link
+              <>
+                <Link
+                  to="/cart"
+                  className="hover:text-amber-200 transition-colors duration-300"
+                >
+                  장바구니
+                </Link>
+                <div className="text-xs pl-1 ml-1 rounded-full bg-red-500 w-4 h-4">
+                  <Link to={"/cart"}>{cartItems.length}</Link>
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
+            {isLogin ? (
+              <li
+                onClick={clickLogout}
+                className="hover:text-amber-200 transition-colors duration-300 cursor-pointer"
+              >
+                로그아웃
+              </li>
+            ) : (
+              <Link
                 to="/member/login"
                 className="hover:text-amber-200 transition-colors duration-300"
               >
                 로그인
               </Link>
-          )}
+            )}
           </ul>
         </nav>
       </div>
