ca6169c69fb8b0fdc4b094fff7b01c973b22a579
soundspeedskye <haneul.skye.cheon@gmail.com>
2024-04-26 14:42:40 +0900

    Update MyListComponent.js

--- front/src/components/community/MyListComponent.js
+++ front/src/components/community/MyListComponent.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import useCustomLogin from "../../hooks/useCustomLogin";
 import { myCommunityList } from "../../api/myListApi";
 
@@ -8,6 +9,7 @@ const initState = [];
 const MyListComponent = () => {
   const [myPosts, setMyPosts] = useState(initState);
   const { isLogin, loginState, exceptionHandle } = useCustomLogin();
+  const navigate = useNavigate();
 
   // Fetch my community list on login state change
   useEffect(() => {
@@ -19,6 +21,10 @@ const MyListComponent = () => {
         .catch((err) => exceptionHandle(err));
     }
   }, [isLogin, loginState.email]);
+
+  const goRead = (bno) => {
+    navigate(`/community/read/${bno}`);
+  };
 
   return (
     <div className="p-8">
@@ -39,8 +45,9 @@ const MyListComponent = () => {
           <ul>
             {myPosts.dtoList.map((post) => (
               <li
+                onClick={() => goRead(post.communityBno)}
                 key={post.communityBno}
-                className="w-full py-8 border-b border-gray-300"
+                className="w-full py-8 border-b border-gray-300 hover:bg-gray-100"
               >
                 <div className="grid grid-cols-1 md:grid-cols-2">
                   <div className="flex w-full py-1">
@@ -49,13 +56,8 @@ const MyListComponent = () => {
                   </div>
                   <div className="flex w-full py-1">
                     <div className="w-1/4">글 제목</div>
-                    <div className="w-3/4">{post.communityTitle}</div>
+                    <div className="w-3/4">{post.communityTitle} </div>
                   </div>
-                  <div className="flex w-full py-1">
-                    <div className="w-1/4">작성자</div>
-                    <div className="w-3/4">{post.communityWriter}</div>
-                  </div>
-                  {/* 추가하고싶은것 추가 */}
                 </div>
               </li>
             ))}
