29da300c5ab6d75d916ed9dae25d8e391025f4c6
dis2211 <0221kmac@gmail.com>
2024-04-29 12:46:11 +0900

    Update QnAPage.js

--- front/src/pages/qna/QnAPage.js
+++ front/src/pages/qna/QnAPage.js
@@ -1,5 +1,5 @@
 import BasicMenu from "../../components/menus/BasicMenu";
-import { FaComment } from "react-icons/fa";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
 import QnAFormComponent from "../../components/qna/QnAFormComponent";
 import FAQComponent from "../../components/qna/FAQComponent";
 
@@ -8,7 +8,7 @@ const QnAPage = () => {
     <div className="flex flex-col w-full">
       <BasicMenu />
       <div className="h-11 bg-emerald-400 bg-gradient-to-b from-emerald-300 via-emerald-400 text-white flex items-center pl-8 sticky top-0 z-50 shadow-md">
-        <FaComment className="w-6 h-6 mr-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]" />
+        <BsFillQuestionCircleFill className="w-6 h-6 mr-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]" />
         <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">문의하기</p>
       </div>
       <div className="w-full h-full">
