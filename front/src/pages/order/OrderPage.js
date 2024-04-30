5a3766234cc68869ffe1a912c75f8fd474cf2458
myj1217 <myj1217@naver.com>
2024-04-30 09:50:43 +0900

    Add Cart CheckList Redux

--- front/src/pages/order/OrderPage.js
+++ front/src/pages/order/OrderPage.js
@@ -1,20 +1,15 @@
 import React from "react";
 import OrderComponent from "../../components/order/OrderComponent";
-import { useParams } from "react-router-dom";
 import BasicMenu from "../../components/menus/BasicMenu";
 import { FaGift } from "react-icons/fa";
 
 const OrderPage = () => {
-  // const { totalPrice } = useParams();
-  //   const { param } = useParams();
-  //   const totalPrice = parseInt(param);
-
   return (
     <div>
       <BasicMenu />
-      <div className="h-11 bg-emerald-500 text-white flex items-center pl-8 sticky top-0 z-50">
-        <FaGift className="w-6 h-6 mr-2" />
-        주문하기
+      <div className="h-11 bg-emerald-400 bg-gradient-to-b from-emerald-300 via-emerald-400 text-white flex items-center pl-8 sticky top-0 z-50 shadow-md">
+        <FaGift className="w-6 h-6 mr-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]" />
+        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">주문하기</p>
       </div>
       <OrderComponent />
     </div>
